d5fc69eb46d4ef6a47634ba487dd4d60dd72424f
wjdsantos <walter4asantos@gmail.com>
2021-03-17 10:47:58 -0300

    Lessons seed, fix: lessonsDone

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Lesson.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Lesson.java
@@ -29,7 +29,7 @@ public abstract class Lesson implements Serializable {
 	private Integer position;
 	
 	@ManyToOne
-	@JoinColumn(name = "lesson_id")
+	@JoinColumn(name = "section_id")
 	private Section section;
 	
 	@ManyToMany
